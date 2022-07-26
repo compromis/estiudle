0e94e55c5cc2ce9fb23aba217a12f22f7753f7e5
lecarles <35672759+lecarles@users.noreply.github.com>
2022-07-26 12:39:21 +0200

    Animate LetterBoard

--- src/components/LetterBoard.vue
+++ src/components/LetterBoard.vue
@@ -3,17 +3,15 @@ import { storeToRefs } from 'pinia'
 import { useBoardStore } from '@/stores/board'
 import { computed } from 'vue'
 
-
 const board = useBoardStore()
 const { maxLetters, letterBoard, finished } = storeToRefs(board)
-
-
 </script>
 
 <template>
   <ul class="letter-board" v-if="!finished">
-    <li v-for="letter in letterBoard" :class="['letter', `letter-${letter.state}`]" :key="letter.letter">
-      {{ letter.letter }}
+    <li v-for="(letter, i) in letterBoard" :class="['letter', `letter-${letter.state}`]" :key="i">
+      <span class="front"></span>
+      <span class="back">{{ letter.letter }}</span>
     </li>
   </ul>
 </template>
@@ -31,23 +29,55 @@ const { maxLetters, letterBoard, finished } = storeToRefs(board)
   transform: scale(.8);
 
   .letter {
-    display: flex;
-    align-items: center;
-    justify-content: center;
+    position: relative;
     margin: 0 .15rem;
     padding: 0;
     height: clamp(2.15rem, 9vw, 5rem);
     width: clamp(1.8rem, 7vw, 4rem);
-    font-size: clamp(1.75rem, 7vw, 3.5rem);
-    background: var(--lightblue);
-    border-radius: var(--border-radius);
-    color: var(--black);
 
-    &-not-in-solution {
-      background: var(--red);
+    .front, .back {
+      font-size: clamp(1.75rem, 7vw, 3.5rem);
+      color: var(--black);
+      border-radius: var(--border-radius);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      position: absolute;
+      top: 0;
+      right: 0;
+      left: 0;
+      bottom: 0;
+      backface-visibility: hidden;
+      transition: .75s ease-out;
     }
 
-    &-empty {
+    .back {
+      transform: rotateY(180deg);
+    }
+
+    &-not-in-solution {
+      .back {
+        background: var(--red);
+        transform: rotateY(0deg);
+      }
+
+      .front {
+        transform: rotateY(180deg);
+      }
+    }
+
+    &-in-solution {
+      .back {
+        background: var(--lightblue);
+        transform: rotateY(0deg);
+      }
+
+      .front {
+        transform: rotateY(180deg);
+      }
+    }
+
+    .front {
       background: var(--white);
       box-shadow: inset 0 0 0 clamp(3px, .25vw, 12px) var(--blue);
     }
