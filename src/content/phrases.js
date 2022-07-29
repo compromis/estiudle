dc2ddab9e8e23d8913823a86d851833acdba9eae
Guillem <guillem@disedit.com>
2022-07-29 11:23:25 +0200

    Add texts

--- src/content/phrases.js
+++ src/content/phrases.js
@@ -2,7 +2,7 @@ export default [
   {
     phrase: 'XORIÇO',
     clue: 'Un corrupte o atemptat a la paella',
-    reveal: 'Si d’alguna cosa sabem les valencianes i els valencians, és del mal que fa el xoriço a la paella i les institucions.'
+    reveal: 'Si d’alguna cosa sabem les valencianes i els valencians, és del mal que fa el xoriço a la paella i a les institucions.'
   },
   {
     phrase: 'RODALIES',
@@ -11,100 +11,103 @@ export default [
   },
   {
     phrase: 'FIGATELL',
-    clue: 'L\'hamburguesa valenciana',
-    reveal: 'Uns diuen que són originals d’Oliva, altres de Pego o Beixama. Siguen d’on siguen, tot el món hauria de tastar alguna vegada en la vida esta delícia tan nostra.'
+    clue: 'L’hamburguesa valenciana',
+    reveal: 'Uns diuen que són originals d’Oliva, altres de Pego o Beixama. Siguen d’on siguen, tot el món hauria de tastar alguna vegada en la vida esta delícia tant nostra.'
   },
   {
     phrase: 'MAGDALENA',
     clue: 'Festa plena',
-    reveal: `Magdalena festa plena, de la llum les festes són,<br>
-    Magdalena festa plena, d’alegria omplen el cor,<br>
-    Magdalena festa plena, les millors festes del món!<br>
-    I com sóc castellonero i com sóc castellonero,<br>
+    reveal: `Magdalena festa plena, de la llum les festes són,<br />
+    Magdalena festa plena, d’alegria omplen el cor,<br />
+    Magdalena festa plena, les millors festes del món!<br />
+    I com soc castellonero i com soc castellonero,<br />
     vull cridar avui ben fort!`
   },
   {
     phrase: 'LA\nBICICLETA',
     clue: 'Grezzi , Ribó i Shakira',
-    reveal: ''
+    reveal: 'Gràcies a la seua aposta per la mobilitat sostenible i l’augment de les connexions ciclistes, València s’ha convertit en una de les ciutats on més persones utilitzen la bicicleta o el patinet per desplaçar-se.'
   },
   {
     phrase: 'AITANA',
     clue: 'En Alacant i alacantina',
-    reveal: ''
+    reveal: 'A més de ser una de les serres més boniques de tot Alacant, és el nom de la nostra Vicepresidenta i Consellera d’Igualtat i Polítiques Inclusives, la tercera consellera alacantina de Compromís.'
   },
   {
     phrase: 'ORXATA',
     clue: 'Me la fiques more',
-    reveal: ''
+    reveal: 'La beguda valenciana de l’estiu per excel·lència i el nom d’un dels col·lectius musicals més autèntics de tot el País Valencià, Orxata Sound System.'
   },
   {
     phrase: 'AEROPORT\nDE L’ALTET',
-    clue: 'El més gran d’Europa sense connexió ferroviaria',
+    clue: 'El més gran d’Europa sense connexió ferroviària',
     reveal: `
-    <p>Sí, no entenem per què, però el govern de Pedro Sánchez, igual que va fer el de Rajoy i tots els anteriors, continuen discriminant a les comarques d’Alacant deixant que l’aeroport de l’Altet d’Alacant-Elx continue sent l’únic de tota Europa sense connexió en tren.</p>
+    <p>Sí, no entenem per què, però el govern de Pedro Sánchez, igual que va fer el de Rajoy i tots els anteriors, continuen discriminant les comarques d’Alacant deixant que l’aeroport de l’Altet d’Alacant-Elx continue sent l’únic de tota Europa sense connexió en tren.</p>
     <p>Però des de Compromís, continuarem reclamant-ho fins que siga una realitat. Més info en <a href="https://trencancelado.com">trencancelado.com</a></p>
     `
   },
   {
-    phrase: 'PLASTICS',
+    phrase: 'PLÀSTICS',
     clue: 'Cada vegada hi ha més a les nostres platges',
-    reveal: ''
+    reveal: 'La contaminació de la mar per microplàstic no només és un gravíssim problema ambiental, sinó de salut pública, que cada any provoca milers de casos de càncer i malalties respiratòries. És per això que apostem per disminuir dràsticament la seua producció, incrementar el reciclatge i perseguir amb tot el pes de la llei a qui considere la nostra Mediterrània el seu abocador particular.'
   },
   {
     phrase: 'TAXA\nTURÍSTICA',
     clue: 'Allò que el turista contribueix en París però no en Benidorm',
-    reveal: ''
+    reveal: `
+      <p>Alguna vegada has deixat de visitar una ciutat per la taxa turística? Doncs sabent que som un dels territoris amb més turisme d’Europa, ha arribat el moment d’enfortir el sector amb una taxa que paguen només qui ens visita i que contribueix a millorar serveis com la neteja, la seguretat, el transport públic, la preservació de les platges o reduir l’impacte ambiental.</p>
+      <p>Més info en <a href="https://taxaturistica.compromis.net/">https://taxaturistica.compromis.net/</a></p>
+    `
   },
   {
-    phrase: 'BOTANIC',
+    phrase: 'BOTÀNIC',
     clue: 'Pese a qui li pese',
-    reveal: ''
+    reveal: 'Govern de coalició d’esquerres triat per la ciutadania que governa la Generalitat des de 2015 i que, per molt que la dreta i l’extrema dreta vulguen amagar, ha millorat i dignificant la vida de les valencianes i els valencians.'
   },
   {
     phrase: 'PARTIDO\nPOPULAR',
     clue: 'Corrupció',
-    reveal: ''
+    reveal: 'L’especialitat de l’únic partit de l’Estat condemnat per corrupció i que, per molt que canvie de cara, continua fent de les magarrufes i els pelotazos per “amiguitos de l’alma” la seua senya d’identitat.'
   },
   {
     phrase: 'JOAN\nBALDOVÍ',
     clue: 'Treballa per 32',
-    reveal: ''
+    reveal: 'Únic diputat valencià al Congrés (dels 32 que es trien) que defensa els interessos de la ciutadania valenciana. La resta sempre ha d’estar pendent del que diga la seu madrilenya del seu partit.'
   },
   {
     phrase: 'ORONELLA',
     clue: 'La teua aliada contra els mosquits',
-    reveal: ''
+    reveal: 'Ocell xicotet que s’alimenta principalment de mosquits que atrapa al vol i que, quan està a prop, és una gran aliada contra les molestes picades de l’estiu.'
   },
   {
     phrase: 'EL MISTERI',
     clue: 'Hui a Elx',
-    reveal: ''
+    reveal: 'Un dels tresors del nostre patrimoni cultural, declarat per la UNESCO Obra Mestra del Patrimoni Oral i Immaterial de la Humanitat i que cada 15 d’agost celebra la seua tradicional Festa en la Basílica de Santa Maria d’Elx. És l’única obra en el seu gènere representada sense interrupcions en tota la seua història. '
   },
   {
     phrase: 'MARIA\nABRADELO',
     clue: 'Babalà',
-    reveal: ''
+    reveal: 'Presentadora de l’antiga Canal 9, popular per conduir durant més de 7 anys el programa infantil ’A la babalà’, així com el programa de karaoke ’Canta Canta’ .'
   },
   {
     phrase: 'NEGOCI',
     clue: 'Abans ho feien amb les ITV, les ressonàncies, els hospitals i ara ja no',
-    reveal: ''
+    reveal: 'Objectiu principal del PP per a enriquir-se i afavorir pelotazos i magarrufes. Com? Privatitzant serveis i fent que la ciutadania pague més del doble per un pitjor servei que si foren públics. Sort que amb Compromís moltes d’estes polítiques especuladores s’estan revertint i retornat a la ciutadania serveis i drets.'
   },
   {
     phrase: 'L’OLIVERA',
     clue: 'El país de...',
-    reveal: ''
+    reveal: 'Cançó del grup valencià Obrint pas escrita pell músic i escriptor Xavi Sarrià, que va merèixer el Premi Ovidi a la millor lletra de cançó.'
   },
   {
     phrase: 'LAWFARE',
     clue: 'Guerra brutal de la dreta i extrema dreta',
-    reveal: ''
+    reveal: 'Anglicisme que explica la utilització per part de la dreta i l’extrema dreta dels recursos judicials per a perjudicar amb denúncies falses els seus rivals polítics i tractar d’aconseguir titulars que publiquen els seus mitjans de comunicació afins per tal de fer creure que “tots els polítics són iguals”. '
   },
   {
     phrase: 'JOAN\nMONLEÓN',
     clue: 'Trie la clotxina o el conill',
-    reveal: ''
+    reveal: 'Actor, cantant, presentador de televisió, un dels principals referents de la cultura valenciana de finals del segle XX i responsable d’un dels programes més exitosos de l’antiga Canal 9, El show de Joan Monleón.'
   },
   {
     phrase: 'INVERSIONS\nDE L’ESTAT',
@@ -123,53 +126,76 @@ export default [
     `
   },
   {
-    phrase: 'LIBERATE',
+    phrase: 'LIBÉRATE',
     clue: '“El que lo prueba repite, yo no sé por qué será”',
-    reveal: ''
+    reveal: `
+      <p>Tot un himne a la llibertat escrit per l’artista Rafael Conde, més conegut com El Titi.</p>
+      <p>Libérate, libérate<br />
+      No vivas más oprimido<br />
+      Busca tu felicidad<br />
+      Que aunque muchos te critiquen<br />
+      El que lo prueba repite<br />
+      Yo no sé por qué será</p>
+    `
   },
   {
     phrase: 'L’ALBUFERA',
     clue: 'L’avarícia del Port se la vol carregar',
-    reveal: ''
+    reveal: `
+      <p>Parc Natural protegit per la Xarxa Natura 2.000 que es troba en perill de salinització per culpa d’empresaris sense escrúpols que volen ampliar el Port de València a qualsevol preu, una infraestructura insostenible que atempta contra el litoral, la nostra economia i la salut de la ciutadania.</p>
+      <p>Més info en <a href="https://portsostenible.com">portsostenible.com</a></p>`
   },
   {
     phrase: 'AULES EN\nBARRACONS',
     clue: 'Hi havia més de 300 i ara estan en perill d’extinció',
-    reveal: ''
+    reveal: 'Per culpa del PP, més de 8.000 xiquets i xiquetes estudiaven en barracons. Per què? Perquè preferien gastar els diners d’educació en mangarrufes. Però amb Compromís, s’han construït més de 39 noves escoles i s’han renovat més de 352 centres educatius fent dels barracons un malson en perill d’extinció.'
   },
   {
     phrase: 'JOAN\nFUSTER',
     clue: 'Forma part d’una bona educació saber en quines ocasions cal ser maleducat',
-    reveal: ''
+    reveal: 'Aforisme d’un dels pensadors valencians més grans de la nostra història, Joan Fuster.'
   },
   {
     phrase: 'PROJECTE\nCASTOR',
     clue: 'L’avarícia de Florentino Pérez va fer tremolar Vinaròs',
-    reveal: ''
+    reveal: `
+      <p>En 2008 el Govern de Zapatero aprovava la posada en marxa del Projecte Castor, un dipòsit submarí d’emmagatzemament de gas. Des del primer moment, col·lectius ecologistes i de defensa del territori varen alertar dels riscos que suposava. També des de Compromís vàrem denunciar aquest projecte i ens hi vàrem oposar.</p>
+      <p>Ací tota la informació de la història d’una GRAN ESTAFA: <a href="https://compromis.net/castor/">compromis.net/castor/</a></p>
+    `
   },
   {
     phrase: 'ESMORZAR',
     clue: 'Sempre amb cacaus i olives',
-    reveal: ''
+    reveal: 'Sens dubte un dels dinars valencians més importants del dia i tota una religió per a les persones matineres que necessiten recuperar forces amb un bon entrepà, acompanyat sempre amb cacaus, olives, tramussos i de postre un bon cremaet.'
   },
   {
     phrase: 'FINANÇAMENT\nJUST',
     clue: 'Allò que mai arriva',
-    reveal: ''
+    reveal: `
+      <p>Sabies que la ciutadania valenciana duu anys i anys sent de les pitjor finançades de tot l’Estat pel govern central? Una injustícia que reconeixen tots els governs centrals, però que ni PP ni PSOE s’han dignat a reformar, malgrat incomplir la Constitució.</p>
+      <p>I que suposa això? Doncs que, malgrat estar per baix de la mitjana estatal de renda per càpita, aportem com si estiguérem per damunt. És a dir, amb els nostres impostos financem territoris més rics que els nostres.</p>
+    `
   },
   {
     phrase: 'ILLA DE\nTABARCA',
     clue: 'Primera Reserva Marina del Mediterrani',
-    reveal: ''
+    reveal: `
+      <p>Sabies que PP, Cs i Vox van votar en contra de la proposta de Compromís de presentar la candidatura per tal què l’illa de Tabarca siga declarada Reserva de la Biosfera per la UNESCO?</p>
+      <p>Malgrat tot continuarem defensant que aquesta meravella natural puga ser protegida contra el turisme massiu, els fondejos de vaixells il·legals i per garantir que la ciutadania de l’illa compte amb els serveis públics que mereixen.</p>
+    `
   },
   {
     phrase: 'LA BANCA',
     clue: 'Sempre guanya',
-    reveal: ''
+    reveal: `
+      <p>La banca privada duu anys aprofitant-se dels diners públics per a repartir dividends i enriquir-se, mentre es nega a tornar els diners del rescat, tanca oficines en pobles i tira persones treballadores al carrer.</p>
+      <p>Per aquest motiu des de Compromís volem que l’Institut Valencià de Finances (IVF) siga la Banca Pública Valenciana, amb oficines en els pobles i atenció personalitzada per a totes i tots, així com canviar la llei de consum perquè els drets dels clients de la banca queden protegits de la seua avarícia.</p>
+      <p>Més info: <a href="https://zerointeres.com/">zerointeres.com</a></p>
+    `
   },
   {
     phrase: 'TOMATINA\nDE BUNYOL',
     clue: 'Fer ketchup a la valenciana',
-    reveal: ''
+    reveal: 'Una de les festes valencianes més divertides i sabroses que se celebra l’últim dimecres d’agost a Bunyol. Es tracta d’una batalla campal on els participants es llancen tomaques i que cada any alberga visitants de tots els punts del planeta, sent una de les festes més internacionals del nostre territori.'
   }
 ]
